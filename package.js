a8f6f0dee2f3d563ffc9ec36f5eceb7b0f5e7486
Denis Perov <denis.perov@gmail.com>
2018-07-02 07:49:31 +0300

    Increment package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'imajus:html-helpers',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'HTML layout Blaze helpers.',
   // URL to the Git repository containing the source code for this package.
