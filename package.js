15c56ebe1776d606ac36b55c68892489ad12f17f
Denis Perov <denis.perov@gmail.com>
2024-08-17 21:35:47 +0500

    Support Meteor 3

--- package.js
+++ package.js
@@ -1,25 +1,37 @@
 Package.describe({
   name: 'imajus:html-helpers',
-  version: '0.0.4',
-  // Brief, one-line summary of the package.
-  summary: 'HTML layout Blaze helpers.',
-  // URL to the Git repository containing the source code for this package.
+  version: '0.1.0',
+  summary: 'HTML layout helpers for Blaze',
   git: 'https://github.com/imajus/meteor-html-helpers.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.4.2');
-  api.use('ecmascript@0.6.3');
-  api.use(['templating@1.3.0', 'underscore@1.0.10', 'ejson@1.0.13', 'underscorestring:underscore.string@3.3.4', 'imajus:helpers-core@0.0.1'], 'client');
+Package.onUse(function (api) {
+  api.versionsFrom(['2.0', '3.0']);
+  api.use('ecmascript');
+  api.use(
+    [
+      'underscore',
+      'ejson',
+      'templating@1.3.4||1.4.4',
+      'imajus:helpers-core@0.1.0',
+    ],
+    'client'
+  );
   api.mainModule('html-helpers.js', 'client');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript@0.6.3');
-  api.use('tinytest@1.0.12');
-  api.use(['templating@1.3.0', 'blaze@2.3.0', 'spacebars@1.0.13', 'ejson@1.0.13', 'imajus:html-helpers'], 'client');
+Package.onTest(function (api) {
+  api.use(['ecmascript', 'tinytest']);
+  api.use(
+    [
+      'templating',
+      'blaze',
+      'spacebars',
+      'ejson',
+      'imajus:html-helpers',
+    ],
+    'client'
+  );
   api.mainModule('html-helpers-tests.js', 'client');
 });
